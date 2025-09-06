090f2d21c24937666ac98ef6fb19483ceae2fdc3
izp1012 <izp1012@naver.com>
2025-09-06 14:46:08 +0900

    init commit.

---
+++ src/main/java/com/meloncity/citiz/CitizApplication.java
@@ -0,0 +1,13 @@
+package com.meloncity.citiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meloncity/citiz/CitizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meloncity.citiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
