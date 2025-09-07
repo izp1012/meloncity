e04ef3d2299d7861d067d8f717493b9e48312a10
suzlnlng <suzlnl@naver.com>
2025-09-07 15:36:42 +0900

    profile 추가

--- src/main/java/com/meloncity/citiz/domain/Post.java
+++ src/main/java/com/meloncity/citiz/domain/Post.java
@@ -1,4 +1,20 @@
 package com.meloncity.citiz.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post {
+    @Id
+    @GeneratedValue
+    private long id;
+
 }
