37e43fa1f90b03016a68908ec2fc164877f0c836
Inhyo <izp1012@naver.com>
2025-09-19 22:52:45 +0900

    PrePersist, PreUpdate 어노테이션 추가

--- src/main/java/com/meloncity/citiz/domain/common/BaseTimeEntity.java
+++ src/main/java/com/meloncity/citiz/domain/common/BaseTimeEntity.java
@@ -1,9 +1,8 @@
 // src/main/java/com/example/domain/common/BaseTimeEntity.java
 package com.meloncity.citiz.domain.common;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 
 import lombok.Getter;
@@ -23,4 +22,16 @@ public abstract class BaseTimeEntity {
     @LastModifiedDate
     @Column(name = "update_date", nullable = false)
     protected LocalDateTime updateDate;
+
+    @PrePersist
+    protected void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        this.createDate = now;
+        this.updateDate = now;
+    }
+
+    @PreUpdate
+    protected void preUpdate() {
+        this.updateDate = LocalDateTime.now();
+    }
 }
