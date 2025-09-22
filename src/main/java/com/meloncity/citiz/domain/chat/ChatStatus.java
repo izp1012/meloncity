8d2e11bf04f431ba5ed4bba20e16929a6b07d426
Inhyo <izp1012@naver.com>
2025-09-23 01:09:02 +0900

    MessageType, ChatStatus 엔트리 추가

--- src/main/java/com/meloncity/citiz/domain/chat/ChatStatus.java
+++ src/main/java/com/meloncity/citiz/domain/chat/ChatStatus.java
@@ -6,7 +6,11 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum ChatStatus {
-    SENT("서버로 전달됨/수신자 받지못함"), DELIVERED("수신자 읽었는지 여부 파악X"), READ("수신자 읽은상태");
+    SENDING("클라이언트에서 전송 중 (임시 상태)"),
+    SENT("서버로 전달됨/수신자 받지못함"),
+    DELIVERED("수신자 읽었는지 여부 파악X"),
+    READ("수신자 읽은상태"),
+    FAILED("전송 실패");
 
-    private String value;
+    private final String value;
 }
