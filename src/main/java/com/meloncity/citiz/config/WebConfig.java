8ed23da56ce19512223a56a2e378826e94384d5c
dlruddnr <spdlqj21tp@gmail.com>
2025-12-01 18:33:22 +0900

    이미지 요청시 맵핑 설정 추가 /img/** 요청시 BaseDir 위치에서 해당 이미지 검색

--- src/main/java/com/meloncity/citiz/config/WebConfig.java
+++ src/main/java/com/meloncity/citiz/config/WebConfig.java
@@ -1,8 +1,10 @@
 package com.meloncity.citiz.config;
 
+import com.meloncity.citiz.util.file.FileStorageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,6 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     private final CorsProperties corsProperties;
+    private final FileStorageService fileStorageService;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -18,5 +21,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/img/**").addResourceLocations("file:" + fileStorageService.getBaseDir());
     }
 }
